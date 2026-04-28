4c250e4de43a77a2601e48c70138c5dbf35d1888
Tarak2903 <tarakkhurana29@gmail.com>
2026-04-28 14:54:55 +0530

    Create checkPaymenService.java

---
+++ FINALEST/checkPaymenService.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+
+public class checkPaymentStatus {
+
+    public boolean paymentStatus(int UserId){
+        return true;
+    }
+}
